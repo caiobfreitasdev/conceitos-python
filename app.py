647e5ac0aa463722a93f6401a8e85f6827620db1
caiobfreitasdev <caiobrenod2@hotmail.com>
2026-08-19 15:27:40 -0300

    fazendo testes com try/execept e if

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import string
 
 def exibir_nome():
     print("""
@@ -14,25 +15,42 @@ def finalizar_app():
     os.system('cls')
     print("Finalizando o app")
 
+def opcao_invalida():
+    print("Opção Inválida!")
+    voltar_menu = input("Deseja voltar ao menu principal? (S/N): \n")
+    if voltar_menu == "S":
+        main()
+    elif voltar_menu == "N":
+        finalizar_app()
+    
+
 def exibir_opcoes():
     print("1. Cadastrar restaurante")
     print("2. Listar restaurante")
     print("3. Ativar restaurante")
     print("4. Sair\n")
+    
 
 
 def selecionar_opcao_programa():
-    selecionar_opcao = int(input("Selecione a opção: "))
-    if selecionar_opcao == 1:
-        print("1. Cadastrar restaurante")
-    elif selecionar_opcao ==2:
-        print("2. Listar restaurante")
-    elif selecionar_opcao ==3:
-        print("3. Ativar restaurante")
-    else:
-        finalizar_app()
+    try:
+        selecionar_opcao = int(input("Selecione a opção: "))
+        print("\n")
+        if selecionar_opcao == 1:
+            print("1. Cadastrar restaurante")
+        elif selecionar_opcao ==2:
+            print("2. Listar restaurante")
+        elif selecionar_opcao ==3:
+            print("3. Ativar restaurante")
+        elif selecionar_opcao ==4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
 
 def main():
+    os.system("cls")
     exibir_nome()
     exibir_opcoes()
     selecionar_opcao_programa()
