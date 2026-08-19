9794baf2baf775a8ca293938211ee44cf1cfeb0c
caiobfreitasdev <caiobrenod2@hotmail.com>
2026-08-19 15:11:48 -0300

    primeiro commit

---
+++ app.py
@@ -0,0 +1,43 @@
+import os
+
+def exibir_nome():
+    print("""
+░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░\n"""
+)
+
+def finalizar_app():
+    os.system('cls')
+    print("Finalizando o app")
+
+def exibir_opcoes():
+    print("1. Cadastrar restaurante")
+    print("2. Listar restaurante")
+    print("3. Ativar restaurante")
+    print("4. Sair\n")
+
+
+def selecionar_opcao_programa():
+    selecionar_opcao = int(input("Selecione a opção: "))
+    if selecionar_opcao == 1:
+        print("1. Cadastrar restaurante")
+    elif selecionar_opcao ==2:
+        print("2. Listar restaurante")
+    elif selecionar_opcao ==3:
+        print("3. Ativar restaurante")
+    else:
+        finalizar_app()
+
+def main():
+    exibir_nome()
+    exibir_opcoes()
+    selecionar_opcao_programa()
+    
+    
+
+if __name__ == '__main__':
+    main()
