da2f36635ac415a8481b395c4ccd33e4267ccac0
caiobfreitasdev <caiobrenod2@hotmail.com>
2026-08-21 08:22:30 -0300

    refatoração da classe Restaurantes para inicialização direta de atributos

--- restaurante.py
+++ restaurante.py
@@ -1,18 +1,22 @@
-
-
 class Restaurantes:
-    nome = ""
-    categoria = ""
-    ativo = False
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
     
-restaurante_praca = Restaurantes()
-restaurante_praca.nome = "praça"
-restaurante_praca.categoria = "Gourmet"
-restaurante_pizza = Restaurantes()
+    def __str__(self):
+        return f"{self.nome} | {self.categoria}"
+    
+restaurante_praca = Restaurantes("praça", "gourmet")
+restaurante_pizza = Restaurantes("pizza", "italiano")
 
 lista_restaurantes = [
     restaurante_pizza,
     restaurante_praca
 ]
 
-print(vars(restaurante_praca))
+print(restaurante_praca)
+print(restaurante_pizza)
+
+
+    
