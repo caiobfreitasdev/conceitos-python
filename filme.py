fb85c63a72c8f16fd635223e83e60238867f9717
caiobfreitasdev <caiobrenod2@hotmail.com>
2026-08-25 14:15:14 -0300

    Complemento da class FilmeNovo

--- filme.py
+++ filme.py
@@ -5,11 +5,25 @@ class FilmeNovo:
         self.ano = ano
         self.duracao_min = duracao_min
         self.assistido = False
-    def __str__(self):
-        return f"""Filme: {self.titulo}\nDiretor: {self.diretor}\nEstreou em {self.ano}, com {self.duracao_min} minutos"""
+
     def marcar_assistido(self):
         self.assistido = True
+    def converter_minuto_em_hora(self):
+        horas, minutos = divmod(self.duracao_min, 60)
+        return f"{horas}h{minutos}"
         
-filme = FilmeNovo("Cidade de Deus", "José Padilla", "2002", "200" )       
+    def __str__(self):
+        if self.marcar_assistido:
+            marcar = "[X]"
+        else:
+            marcar = "[ ]"
+        return f"{marcar} Filme: {self.titulo}\nDiretor: {self.diretor}\nEstreou em {self.ano}, com {self.converter_minuto_em_hora()}"
+
+filme = FilmeNovo("Cidade de Deus", "José Padilla", 2002, 200 )       
+
+
 
 print(filme) 
+filme.marcar_assistido()
+print(filme) 
+
